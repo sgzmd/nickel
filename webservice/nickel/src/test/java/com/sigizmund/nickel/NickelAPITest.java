346679e3a58f1c1ca543fda5bba8d83954c5fa37
Roman Kirillov <sigizmund@gmail.com>
2014-07-16 14:07:50 +0100

    Fixing tests

--- webservice/nickel/src/test/java/com/sigizmund/nickel/NickelAPITest.java
+++ webservice/nickel/src/test/java/com/sigizmund/nickel/NickelAPITest.java
@@ -10,33 +10,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class NickelAPITest {
-  private static final String HTML;
-  static {
-    try {
-      HTML = CharStreams.toString(new BufferedReader(new InputStreamReader(
-          NickelAPITest.class.getResourceAsStream("/test.html"))));
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+    private static final String HTML;
+    static {
+        try {
+            HTML = CharStreams.toString(new BufferedReader(new InputStreamReader(
+                    NickelAPITest.class.getResourceAsStream("/test.html"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-  }
-  private static final String URL = "https://www.fanfiction.net/s/3384712/1/The-Lie-I-ve-Lived";
+    private static final String URL = "https://www.fanfiction.net/s/3384712/1/The-Lie-I-ve-Lived";
 
-  @Test
-  public void smokeTest() {
-    NickelAPI api = new NickelAPI();
-  }
+    @Test
+    public void smokeTest() {
+        NickelAPI api = new NickelAPI();
+    }
 
-  @Test
-  public void initDownloadTest() throws IOException {
-    NickelAPI api = new NickelAPI();
-    StartBookDownloadResponse resp = api.startBookDownload(URL, HTML);
-    Truth.ASSERT
-        .that(resp.getRequestedChapterUrls().size())
-        .is(24);
+    @Test
+    public void initDownloadTest() throws IOException {
+        NickelAPI api = new NickelAPI();
+        StartBookDownloadResponse resp = api.startBookDownload(URL, HTML);
+        Truth.ASSERT
+                .that(resp.getRequestedChapterUrls().size())
+                .is(24);
 
-    // spot check
-    Truth.ASSERT
-        .that(resp.getRequestedChapterUrls().get(1))
-        .is("https://www.fanfiction.net/s/3384712/2/The-Lie-I-ve-Lived");
-  }
+        // spot check
+        Truth.ASSERT
+                .that(resp.getRequestedChapterUrls().get(1))
+                .is("https://www.fanfiction.net/s/3384712/2/The-Lie-I-ve-Lived");
+    }
 }
